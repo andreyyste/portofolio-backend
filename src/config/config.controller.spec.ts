d7668e62cbc29cf71a82034dfa13324f3b9fa11b
andreyyste <manurungandre1927@gmail.com>
2026-06-05 04:54:11 +0700

    feat(backend-test): mock CACHE_MANAGER in ConfigController spec

--- src/config/config.controller.spec.ts
+++ src/config/config.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ConfigController } from './config.controller';
 import { ConfigService } from './config.service';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
 
 describe('ConfigController', () => {
   let controller: ConfigController;
@@ -12,6 +13,14 @@ describe('ConfigController', () => {
         {
           provide: ConfigService,
           useValue: {},
+        },
+        {
+          provide: CACHE_MANAGER,
+          useValue: {
+            get: jest.fn(),
+            set: jest.fn(),
+            del: jest.fn(),
+          },
         },
       ],
     }).compile();
